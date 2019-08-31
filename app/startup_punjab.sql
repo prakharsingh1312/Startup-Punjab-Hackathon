22ccb28daa42dbe6238759357d8fedf6adef4e49
Divya Prakash Mittal <divyaprakashmittal@gmail.com>
2019-08-31 16:35:38 +0530

    Populated DB

--- app/startup_punjab.sql
+++ app/startup_punjab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 31, 2019 at 02:40 PM
+-- Generation Time: Aug 31, 2019 at 04:34 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.2
 
@@ -23,75 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `announcements`
---
-
-CREATE TABLE `announcements` (
-  `id` int(11) NOT NULL,
-  `n_id` int(11) NOT NULL,
-  `title` longtext NOT NULL,
-  `content` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `city`
---
-
-CREATE TABLE `city` (
-  `id` int(11) NOT NULL,
-  `name` longtext NOT NULL,
-  `pincode` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `industry`
---
-
-CREATE TABLE `industry` (
-  `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `industry_sector`
---
-
-CREATE TABLE `industry_sector` (
-  `i_id` int(11) NOT NULL,
-  `sec_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `nature`
---
-
-CREATE TABLE `nature` (
-  `id` int(11) NOT NULL,
-  `name` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sector`
---
-
-CREATE TABLE `sector` (
-  `id` int(11) NOT NULL,
-  `name` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `services`
 --
 
@@ -100,81 +31,37 @@ CREATE TABLE `services` (
   `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `stage`
+-- Dumping data for table `services`
 --
 
-CREATE TABLE `stage` (
-  `id` int(11) NOT NULL,
-  `name` longtext
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `startups`
---
-
-CREATE TABLE `startups` (
-  `id` int(11) NOT NULL,
-  `logo` varchar(30) NOT NULL,
-  `name` mediumtext NOT NULL,
-  `brief` longtext NOT NULL,
-  `stage_id` int(11) NOT NULL,
-  `c_id` int(11) NOT NULL,
-  `funded` int(11) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `mobile` int(11) NOT NULL,
-  `website` varchar(30) NOT NULL,
-  `app_link` varchar(30) NOT NULL,
-  `i_id` int(11) NOT NULL,
-  `sec_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `services` (`id`, `name`) VALUES
+(1, 'Consumer Internet'),
+(2, 'Discovery'),
+(3, 'E-commerce'),
+(4, 'Engineering'),
+(5, 'Enterprise Mobility'),
+(6, 'Government'),
+(7, 'Hyperlocal'),
+(8, 'Location Based Services'),
+(9, 'Manufacturing'),
+(10, 'Marketplace'),
+(11, 'Mobile'),
+(12, 'Offline'),
+(13, 'Online Aggregator'),
+(14, 'Peer to Peer'),
+(15, 'Platform'),
+(16, 'Rental'),
+(17, 'Research'),
+(18, 'SaaS'),
+(19, 'Sharing Economy'),
+(20, 'Social Enterprise'),
+(21, 'Subscription Commerce'),
+(22, 'Others');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `announcements`
---
-ALTER TABLE `announcements`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_n_id` (`n_id`);
-
---
--- Indexes for table `city`
---
-ALTER TABLE `city`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `industry`
---
-ALTER TABLE `industry`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `industry_sector`
---
-ALTER TABLE `industry_sector`
-  ADD KEY `fk_i_id` (`i_id`),
-  ADD KEY `fk_sec_id` (`sec_id`);
-
---
--- Indexes for table `nature`
---
-ALTER TABLE `nature`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `sector`
---
-ALTER TABLE `sector`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `services`
@@ -183,91 +70,14 @@ ALTER TABLE `services`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `stage`
---
-ALTER TABLE `stage`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `startups`
---
-ALTER TABLE `startups`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `f_s_id` (`stage_id`),
-  ADD KEY `f_sec_id` (`sec_id`),
-  ADD KEY `f_i_id` (`i_id`),
-  ADD KEY `f_c_id` (`c_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `announcements`
---
-ALTER TABLE `announcements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `city`
---
-ALTER TABLE `city`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `industry`
---
-ALTER TABLE `industry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `nature`
---
-ALTER TABLE `nature`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `sector`
---
-ALTER TABLE `sector`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `stage`
---
-ALTER TABLE `stage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `startups`
---
-ALTER TABLE `startups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `announcements`
---
-ALTER TABLE `announcements`
-  ADD CONSTRAINT `fk_n_id` FOREIGN KEY (`n_id`) REFERENCES `nature` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `industry_sector`
---
-ALTER TABLE `industry_sector`
-  ADD CONSTRAINT `fk_i_id` FOREIGN KEY (`i_id`) REFERENCES `industry` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_sec_id` FOREIGN KEY (`sec_id`) REFERENCES `sector` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `startups`
---
-ALTER TABLE `startups`
-  ADD CONSTRAINT `f_c_id` FOREIGN KEY (`c_id`) REFERENCES `city` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `f_i_id` FOREIGN KEY (`i_id`) REFERENCES `industry` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `f_s_id` FOREIGN KEY (`stage_id`) REFERENCES `stage` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `f_sec_id` FOREIGN KEY (`sec_id`) REFERENCES `sector` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- app/views/main.py
+++ app/views/main.py
@@ -9,6 +9,7 @@ from flask import (Blueprint, Flask, flash, g, redirect, render_template,
                    request, send_file, session, url_for)
 from flask_mysqldb import MySQL
 from flask_session import Session
+import csv
 # from passlib.hash import sha256_crypt as sha
 
 main = Blueprint('main', __name__)
@@ -16,3 +17,14 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template("dashboard.html", **locals())
+
+@main.route('/populate')
+def populate():
+    with open('services.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count > 0:
+                execute_db("INSERT INTO services(name) values(%s);", (row["services"], ))
+            line_count += 1
+    return "Success"
